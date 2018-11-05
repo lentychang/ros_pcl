614438cfc9278f1346d3c22214bc87f0ea41339e
Lenty Chang <lentychang@gmail.com>
2018-11-05 10:44:36 +0100

    add segmentation function

--- include/pcl_objRec/impl/utils.cpp
+++ include/pcl_objRec/impl/utils.cpp
@@ -1,6 +1,4 @@
-#include <pcl/io/io.h>
-#include <iostream>
-#include <pcl/search/kdtree.h>
+#include <pcl_objRec/utils.h>
 
 
 double
@@ -9,21 +7,28 @@ computeCloudResolution(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
 	double resolution = 0.0;
 	int numberOfPoints = 0;
 	int nres;
-	std::vector<int> indices(3);
-	std::vector<float> squaredDistances(3);
+	std::vector<int> indices(11);
+	std::vector<float> squaredDistances(11);
 	pcl::search::KdTree<pcl::PointXYZ> tree;
 	tree.setInputCloud(cloud);
-
+	int j = 1;
+	bool flag = true;
 	for (size_t i = 0; i < cloud->size(); ++i)
 	{
 		if (! pcl_isfinite((*cloud)[i].x))
 			continue;
 
 		// Considering the second neighbor since the first is the point itself.
-		nres = tree.nearestKSearch(i, 2, indices, squaredDistances);
-		if (nres == 2)
+		j = 1;
+		nres = tree.nearestKSearch(i, j + 1, indices, squaredDistances);
+		while (squaredDistances[j] < 0.00000001 || j > 10)
 		{
-			resolution += sqrt(squaredDistances[1]);
+			j += 1;
+			nres = tree.nearestKSearch(i, j + 1, indices, squaredDistances);
+			if (j ==10) flag = false; 
+		}
+		if (flag){
+			resolution += sqrt(squaredDistances[j]);
 			++numberOfPoints;
 		}
 	}
@@ -37,4 +42,68 @@ computeCloudResolution(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
 	}
 		
 	return resolution;
+}
+
+void
+mls_upsampling(pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr &outCloud, float resolution ,float stepRatio){
+	// Filtering object.
+	pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointXYZ> filter;
+	filter.setInputCloud(inCloud);
+	// Object for searching.
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree;
+	filter.setSearchMethod(kdtree);
+	// Use all neighbors in a radius of 3cm.
+	filter.setSearchRadius(resolution * 2.5);
+	// Upsampling method. Other possibilites are DISTINCT_CLOUD, RANDOM_UNIFORM_DENSITY
+	// and VOXEL_GRID_DILATION. NONE disables upsampling. Check the API for details.
+	filter.setUpsamplingMethod(pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointXYZ>::SAMPLE_LOCAL_PLANE);
+	// Radius around each point, where the local plane will be sampled.
+	filter.setUpsamplingRadius(resolution * 2.5);
+	// Sampling step size. Bigger values will yield less (if any) new points.
+	filter.setUpsamplingStepSize(resolution * stepRatio);
+	filter.process(*outCloud);
+}
+
+void
+euclideanSeg (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int minClusterSize, int maxClusterSize, float cluster_tol, bool enableViewer)
+{
+    // Create EuclideanClusterExtraction and set parameters
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (cluster_tol);
+    ec.setMinClusterSize (minClusterSize);
+    ec.setMaxClusterSize (maxClusterSize);
+    // set input cloud and let it run
+    std::vector <pcl::PointIndices> clusters;
+    ec.setInputCloud (cloud);
+    ec.extract (clusters);
+
+    if (static_cast<int>(clusters.size()==0)){
+        ROS_WARN("No cluster found!!");
+        saveFile_ = false;
+    }
+
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pntsInCluster (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PointIndices::Ptr pntIdxs (new pcl::PointIndices ());    
+    std::string filename;
+
+	pcl::visualization::PCLVisualizer viewer ("Cluster viewer");
+
+    if (enableViewer){
+        for (int i=0;i<static_cast<int>(clusters.size());++i){
+
+            extract.setInputCloud (cloud);
+            *pntIdxs = clusters[i];
+            extract.setIndices(pntIdxs);
+            extract.setNegative (false);
+            extract.filter(*pntsInCluster);
+
+			viewer.addPointCloud (pntsInCluster, "clusterCloud");
+			while(!viewer.wasStopped()) {
+				viewer.spinOnce ();
+			}
+			viewer.removeAllPointClouds();
+			viewer.resetStoppedFlag();
+        }
+    }
 }
--- include/pcl_objRec/utils.h
+++ include/pcl_objRec/utils.h
@@ -3,9 +3,30 @@
 #include <pcl/io/io.h>
 #include <iostream>
 #include <pcl/search/kdtree.h>
+#include <pcl/surface/mls.h>
+
+#include <vector>
+#include <ros/ros.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/console/parse.h>
+#include <pcl/segmentation/extract_clusters.h>
+
 
 
 double
 computeCloudResolution(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud);
+
+void
+mls_upsampling(pcl::PointCloud<pcl::PointXYZ>::Ptr& inCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr &outCloud, float resolution ,float stepRatio=0.5);
+
+void 
+euclideanSeg (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int minClusterSize=30, int maxClusterSize=1500, float cluster_tol=0.01f, bool enableViewer=false);
 
 #endif
